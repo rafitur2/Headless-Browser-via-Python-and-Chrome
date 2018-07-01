1af6cadfeef03b527745af288c52fa630acc40fa
Rafi Diamant2 <rafi.diamant@amdocs.com>
2018-07-01 16:00:59 +0300

    comment

--- Test_Headless.py
+++ Test_Headless.py
@@ -22,22 +22,24 @@ class Test_Headless(unittest.TestCase):
 
     def test_google_GUI(self):
 
+        print("Starting GUI Browser Testing")
         start =time.time()
         wd.get("http://www.google.com")
-        # Simple testing webpage title
         assert "Google" in wd.title
         if "Google" in wd.title:
             print("GUI browser testing has passed")
         else:
             print("GUI browser testing has passed failed")
-        # print ("Screen Shot was taken")
-        wd.get_screenshot_as_file("ScreenShot.png")
+        print ("Screen Shot was taken for GUI")
+        wd.get_screenshot_as_file("ScreenShotGUI.png")
         wd.close()
         wd.quit()
         end=time.time()
         print("GUI Performance time is:",end-start)
+        print("---------------------------------------------")
 
     def test_google_head_less(self):
+        print("Starting headless Browser Testing")
         chrome_options.add_argument("--headless")
         wd = webdriver.Chrome(options=chrome_options)
         start =time.time()
@@ -48,12 +50,13 @@ class Test_Headless(unittest.TestCase):
             print("Headless browser testing has passed")
         else:
             print("Headless browser testing has passed failed")
-        # print ("Screen Shot was taken")
-        wd.get_screenshot_as_file("ScreenShot.png")
+        print ("Screen Shot was taken for headless")
+        wd.get_screenshot_as_file("ScreenShot_headless.png")
         wd.close()
         wd.quit()
         end=time.time()
         print("Headless Performance time is:",end-start)
+
 
 if __name__ == '__main__':
     unittest.main()
