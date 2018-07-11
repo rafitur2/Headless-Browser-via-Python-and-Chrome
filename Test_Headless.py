81a76905fe7675f485f0492575ee1a6c12cd5971
Rafi Diamant2 <rafi.diamant@amdocs.com>
2018-07-11 10:49:08 +0300

    comment

--- Test_Headless.py
+++ Test_Headless.py
@@ -1,68 +1,22 @@
-import os  
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-import pytest
-import unittest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 import time
 
-chrome_options = Options()
-
-##Using headless option
 
 
-
-# chrome_options.binary_location = '/Applications/Google Chrome   Canary.app/Contents/MacOS/Google Chrome Canary'
-
-# wd = webdriver.Chrome(chrome_options=chrome_options)
-
-wd = webdriver.Chrome()
-
-class Test_Headless(unittest.TestCase):
-
-    def test_google_GUI(self):
-
-        print("Starting GUI Browser Testing")
-        start =time.time()
-        wd.get("http://www.google.com")
-        assert "Google" in wd.title
-        if "Google" in wd.title:
-            print("GUI browser testing has passed")
-        else:
-            print("GUI browser testing has passed failed")
-        print ("Screen Shot was taken for GUI")
-        wd.get_screenshot_as_file("ScreenShotGUI.png")
-        wd.close()
-        wd.quit()
-        end=time.time()
-        print("GUI Performance time is:",end-start)
-        print("---------------------------------------------")
-
-    def test_google_head_less(self):
-        print("Starting headless Browser Testing")
-        chrome_options.add_argument("--headless")
-        wd = webdriver.Chrome(options=chrome_options)
-        start =time.time()
-        wd.get("http://www.google.com")
-        # Simple testing webpage title
-        assert "Google" in wd.title
-        if "Google" in wd.title:
-            print("Headless browser testing has passed")
-        else:
-            print("Headless browser testing has passed failed")
-        print ("Screen Shot was taken for headless")
-        wd.get_screenshot_as_file("ScreenShot_headless.png")
-        wd.close()
-        wd.quit()
-        end=time.time()
-        print("Headless Performance time is:",end-start)
-
-
-if __name__ == '__main__':
-    unittest.main()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
 
 
 
-
-
-
+Value = "Under 20"
+driver.get("http://www.teachmeselenium.com/automation-practice/")
+time.sleep(10)
+dropdown = Select(driver.find_element_by_name("age"))
+dropdown.select_by_value(Value)
